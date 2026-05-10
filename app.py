0e2d03f794a35ea07e7497348e40c439a59e0dfd
tnhl <tnhl.us@gmail.com>
2026-05-10 11:21:33 +0700

    Dashboard

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 import streamlit as st
-import sys, pathlib
+import sys
+import pathlib
+
 sys.path.insert(0, str(pathlib.Path(__file__).parent))
 
 st.set_page_config(
-    page_title="Shopee Mỹ phẩm VN – Lab 01",
+    page_title="Bất động sản Việt Nam 2024",
     layout="wide",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="expanded",
 )
 
-pg = st.navigation([
-    st.Page("pages/Home.py", title="Tổng quan"),
-    st.Page("pages/EDA.py", title="Mô tả dữ liệu"),
-    st.Page("pages/01_Pricing.py", title="Giá & Danh mục"),
-    st.Page("pages/02_Reviews.py", title="Đánh giá & Cảm xúc khách hàng"),
-    st.Page("pages/03_Market.py", title="Phân khúc Thị trường"),
-    st.Page("pages/04_Shops.py", title="Hiệu quả Cửa hàng"),
-    st.Page("pages/05_Geo_Mall.py", title="Địa lý & Shop Mall"),
-    ])
+pg = st.navigation(
+    [
+        # st.Page("pages/Toan.py", title="Cấu trúc & Quy mô"),
+        st.Page("pages/Dia_ly.py", title="Phân tích Địa lý"),
+    ]
+)
 pg.run()
